07587de6a56da5f1d139d122c4f196695bd08b41
BexultanAkhmetzhanov <vnqpc6z5rw@privaterelay.appleid.com>
2025-10-24 12:56:23 +0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://ig-parser-project.fly.dev';
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
